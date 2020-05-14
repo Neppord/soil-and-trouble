b717ce3927dc9b5c6b41dbeb6a22ec5b6765e5b4
Samuel Ytterbrink <neppord@gmail.com>
2020-05-14 18:21:10 +0200

    Add pixel engine and structure

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,43 @@
 module Main exposing (..)
 
+import Debug exposing (todo)
 import Html exposing (text)
+import PixelEngine exposing (game)
 
 
 main =
-    text "Hello world"
+    game
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        , controls = controls
+        , width = width
+        }
 
 
-type MyType
-    = TypeA
-    | TypeB
-    | TypeC String
+init () =
+    ( (), Cmd.none )
+
+
+update _ model =
+    ( model, Cmd.none )
+
+
+subscriptions _ =
+    Sub.none
+
+
+view _ =
+    { title = "Soil and Trouble"
+    , options = Nothing
+    , body = []
+    }
+
+
+controls _ =
+    Nothing
+
+
+width =
+    600
